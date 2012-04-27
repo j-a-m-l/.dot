b3e0db3e67d1d54bfb57d456650268c44bb3ad00
Juan <ju.an.marluc@gmail.com>
2012-04-27 11:03:42 +0200

    Emacs

---
+++ emacs.d/init.el
@@ -0,0 +1,31 @@
+;; movement key bindings (like vi)
+;;(global-set-key "\C-l" 'forward-char)
+;;(global-set-key "\C-h" 'backward-char)
+;;(global-set-key "\C-j" 'next-line)
+;;(global-set-key "\C-k" 'previous-line)
+
+(set-face-foreground 'vertical-border "white")
+(set-face-background 'vertical-border "white")
+
+;; org-mode
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+;; track when a task is finished
+(setq org-log-done 'time)
+;(setq org-default-notes-file "notes.org")
+
+;; mail TODO
+
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(initial-buffer-choice "~/.dot.private/no.org")
+ '(org-agenda-files (quote ("~/.dot.private/no.org"))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
