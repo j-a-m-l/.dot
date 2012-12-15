5c20d9cf50731d43e8c5b3b74d536230c84ec039
noijd <ju.an.marluc@gmail.com>
2012-12-15 13:58:42 +0100

    Move and add some shell aliases

---
+++ aliases.sh
@@ -0,0 +1,16 @@
+# ls
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Vim
+alias v='vim'
+
+# Grep
+alias gi='grep -i'
+
+# Apt
+alias sagi='sudo apt-get install'
+
+# Ruby
+alias rb='ruby'
