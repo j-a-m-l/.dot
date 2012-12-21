8c0a31147be1824380c5c262b3cc90f26d55fa9a
noijd <ju.an.marluc@gmail.com>
2012-12-21 10:42:59 +0100

    Add keybindings to XMonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,25 @@
 import XMonad
+-- For key bindings
+import qualified Data.Map as M
 
-main = xmonad defaultConfig
+main = xmonad defaults
 
-	-- Use super instead of alt as the modkey
-    {modMask = mod4Mask
-    }
+defaults = defaultConfig
+		-- Use super instead of alt as the modkey
+		{ modMask = mod4Mask
+		, borderWidth = 2
+		, normalBorderColor = "#4488FF"
+		, focusedBorderColor = "#FF4455"
+		, workspaces = myWorkSpaces 
+		, keys = \c -> myKeys c `M.union` keys defaultConfig c
+		}
+
+	where
+
+	myWorkSpaces = ["1:launcher", "2:web", "3:development"]
+
+	myKeys (XConfig {modMask = modm}) = M.fromList $
+		[ ((mod4Mask, xK_f), spawn "firefox")
+		, ((mod4Mask, xK_v), spawn "gvim")
+		, ((mod4Mask, xK_e), spawn "emacs")
+		]
