525abf145b942946ec53f06b321b6f9cfc6a562d
noijd <ju.an.marluc@gmail.com>
2012-12-29 10:32:29 +0100

    Change workspaces and border color; add Amarok shortcut

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -8,18 +8,19 @@ defaults = defaultConfig
 		-- Use super instead of alt as the modkey
 		{ modMask = mod4Mask
 		, borderWidth = 2
-		, normalBorderColor = "#4488FF"
-		, focusedBorderColor = "#FF4455"
+		, normalBorderColor = "#000"
+		, focusedBorderColor = "#4488FF"
 		, workspaces = myWorkSpaces 
 		, keys = \c -> myKeys c `M.union` keys defaultConfig c
 		}
 
 	where
 
-	myWorkSpaces = ["1:launcher", "2:web", "3:development"]
+	myWorkSpaces = ["1:launcher", "2:web", "3:code", "4:helper", "5:communication", "6:learn", "7:music", "8:networks"]
 
 	myKeys (XConfig {modMask = modm}) = M.fromList $
 		[ ((mod4Mask, xK_f), spawn "firefox")
 		, ((mod4Mask, xK_v), spawn "gvim")
 		, ((mod4Mask, xK_e), spawn "emacs")
+		, ((mod4Mask, xK_a), spawn "amarok")
 		]
