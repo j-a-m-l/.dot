43f2bb281e17742b6dafeb1c97435b0d228929ea
noijd <ju.an.marluc@gmail.com>
2013-03-09 19:03:30 +0100

    Improve configuration
     » Add more workspaces
     » Start the application in workspaces
     » Change Shift + c to Escape
     » Set Tab for switching screens

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,31 +1,66 @@
 import XMonad
 -- For key bindings
 import qualified Data.Map as M
+import qualified XMonad.StackSet as W
+-- For cycling screens
+import qualified XMonad.Actions.CycleWS as CWS
+-- Java Swing
+import XMonad.Hooks.SetWMName
 
-main = xmonad defaults
+-- Send applications to their dedicated Workspace
+myManageHook = composeAll
+                [ resource  =? "gvim"             --> doShift "1:editor"
+				, resource  =? "chromium-browser" --> doShift "2:web"
+				, className =? "Firefox"          --> doShift "2:web"
+				, resource  =? "dolphin"          --> doShift "6:explorer"
+				, resource  =? "amarok"           --> doShift "7:music"
+				, resource  =? "transmission-gtk" --> doShift "8:network"
+				, className =? "Thunderbird"      --> doShift "9:communication"
+				, resource  =? "emacs"            --> doShift "0:org"
+                ]
 
-defaults = defaultConfig
+main = do
+	xmonad defaultConfig
+		{ manageHook = myManageHook <+> manageHook defaultConfig
 		-- Use super instead of alt as the modkey
-		{ modMask = mod4Mask
+		, modMask = mod4Mask
 		, borderWidth = 2
 		, normalBorderColor = "#000"
 		, focusedBorderColor = "#4488FF"
 		, workspaces = myWorkSpaces 
 		, keys = \c -> myKeys c `M.union` keys defaultConfig c
+		-- Fix Java Swing??
+		, startupHook = setWMName "LG3D"
 		}
 
-	where
+		where
 
-	myWorkSpaces = ["1:launcher", "2:web", "3:code", "4:helper", "5:communication", "6:learn", "7:music", "8:networks", "9:org"]
+		myWorkSpaces = ["`:home", "1:editor", "2:web", "3:console", "4:daemons", "5:helper", "6:explorer", "7:music", "8:network", "9:communication", "0:org", "-:limbo"]
 
-	myKeys (XConfig {modMask = modm}) = M.fromList $
-		[ ((mod4Mask, xK_f), spawn "firefox")
-		, ((mod4Mask, xK_v), spawn "gvim")
-		, ((mod4Mask, xK_e), spawn "emacs")
-		, ((mod4Mask, xK_a), spawn "amarok")
-		, ((mod4Mask, xK_t), spawn "transmission-gtk")
-		, ((mod4Mask, xK_c), spawn "chromium-browser")
-		, ((mod4Mask, xK_i), spawn "anki")
-		, ((mod4Mask, xK_b), spawn "calibre")
-		, ((mod4Mask, xK_g), spawn "tuxguitar")
-		]
+		-- Keys for the workspaces
+		workSpacesKeys = [xK_quoteleft, xK_1, xK_2, xK_3, xK_4, xK_5, xK_6, xK_7, xK_8, xK_9, xK_0, xK_minus]
+
+		myKeys conf@(XConfig {modMask = modm}) = M.fromList $
+			[ ((modm .|. shiftMask, xK_c), return ())
+			-- Use Esc instead of Shift + c
+			, ((modm, xK_Escape), kill)
+			-- Move focus to other Xinerama screen.
+			, ((modm, xK_Tab), CWS.nextScreen)
+			, ((modm, xK_v), spawn "gvim -f")
+			, ((modm, xK_e), spawn "emacs")
+			, ((modm, xK_f), spawn "firefox")
+			, ((modm, xK_c), spawn "chromium-browser")
+			, ((modm, xK_a), spawn "amarok")
+			, ((modm, xK_u), spawn "thunderbird")
+			, ((modm, xK_t), spawn "transmission-gtk")
+			, ((modm, xK_b), spawn "calibre")
+			, ((modm, xK_g), spawn "tuxguitar")
+			, ((modm, xK_d), spawn "dolphin")
+			, ((modm, xK_i), spawn "anki")
+			]
+			++
+			-- mod-n, Switch to workspace N
+			-- mod-shift-n, Move client to workspace N
+			[((m .|. modm, k), windows $ f i)
+				| (i, k) <- zip (XMonad.workspaces conf) workSpacesKeys
+				, (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
