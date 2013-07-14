cb9492f1077ef1489a1e7b3ce62e32087919a53a
noijd <ju.an.marluc@gmail.com>
2013-07-14 03:39:10 +0200

    Exchange tiny Win and big Alt

--- xmonad/xmonadmap.sh
+++ xmonad/xmonadmap.sh
@@ -6,3 +6,8 @@ setxkbmap -layout us -variant altgr-intl
 xmodmap -e "remove Lock = Caps_Lock"
 xmodmap -e "keysym Caps_Lock = Control_L"
 xmodmap -e "add Control = Control_L"
+# Exchange Left Alt and Win
+xmodmap -e "remove mod1 = Alt_L"
+xmodmap -e "remove mod4 = Super_L"
+xmodmap -e "add mod1 = Super_L"
+xmodmap -e "add mod4 = Alt_L"
