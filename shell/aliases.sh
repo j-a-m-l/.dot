2b3b20548a99db8a3c4a5a3296b830dcc501c753
noijd <ju.an.marluc@gmail.com>
2014-03-19 13:34:24 +0000

    More aliases and fix zsh conflict with rake

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,24 +1,51 @@
-# ls
-alias l='ls'
-alias ll='ls -alF'
-alias la='ls -A'
-alias lg='ls | grep -i'
+#!/bin/sh
 
-# Vim
-alias v='vim'
-
-# grep
-alias ge='grep -i'
+shell=`ps | grep $$ | awk '{ print $4 }'`
 
 # apt
 alias sagi='sudo apt-get install'
 alias sagu='sudo apt-get update'
 
-# Ruby
-alias rb='ruby'
+# Bundle
+alias be='bundle exec'
 
 # git
 alias g='git'
+
+# grep
+alias ge='grep -i'
+
+# Grunt
+alias grach='grunt && grunt watch'
+
+# ls
+alias l='ls'
+alias ll='ls -alF'
+alias la='ls -A'
+alias lg='ls -a | grep -i'
+
+# Open like a boss. I really need it...
+alias o='__open__'
+
+# Ruby
+alias rb='ruby'
+
+# Rake
+# For avoiding that zsh tries to autocomplete something like "rake task[arg]"
+if [ 'zsh' = $shell ]; then
+  alias rake='noglob rake'
+fi
+alias rk='rake'
+
+# Shutting down the easy way
+alias Shutdown='sudo shutdown -h -P now'
+alias Restart='sudo shutdown -r now'
+
+# SimpleHttpServer
+alias simple_server='python -m SimpleHTTPServer'
+
+# Tmuxinator
+alias tm='tmuxinator'
 
 # Python
 alias py='python'
@@ -26,19 +53,16 @@ alias ipy='ipython'
 
 # Vagrant
 alias vag='vagrant'
-alias vago='vagrant up && vagrant ssh'
-alias vagend='vagrant suspend'
+alias vags='vagrant ssh'
+alias vago='__vagopen__'
+alias vend='vagrant suspend'
 
-# I really need it...
-# gnome-open
-open() {
+# Vim
+alias v='vim'
+
+__open__() {
 	nohup xdg-open $1 > /dev/null 2>&1 &
 }
-alias o='open'
-
-# Easy way
-alias Shutdown='sudo shutdown -h -P now'
-alias Restart='sudo shutdown -r now'
-
-# Private alias (paths, etc.)
-. ~/.dot.private/shell/aliases.sh
+__vagopen__() {
+	vagrant up $1 && vagrant ssh $1
+}
