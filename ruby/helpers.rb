2f1379b27f8e8e67a285d62da2599f71b181026a
noijd <ju.an.marluc@gmail.com>
2015-09-01 09:42:10 +0100

    Some Ruby improvements

---
+++ ruby/helpers.rb
@@ -0,0 +1,14 @@
+# Toys
+class C; self; end
+def s; 'Example string' end
+def a; ['a', 'b', 'c', 'd'] end
+def h; { a: 'a', b: 'b', c: 'c', d: 'd' } end
+def hn; { a: { b: { c: { d: 'E' } } } } end
+
+def save_current_session path
+  File.open path, 'w' do |f|
+    f << Readline::HISTORY.to_a.join("\n")
+  end
+end
+
+def q; quit end
