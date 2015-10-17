c1270538590823f90916b3e8c42e47b861c424e6
jaml <dev@jaml.site>
2015-10-17 10:02:34 +0100

    Add aliases for cleaning Docker images and containers

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -24,6 +24,13 @@ alias doa='docker attach'
 alias dor='docker run'
 alias dori='docker run -it'
 alias dob='docker build'
+alias doi_clean='docker rmi `docker images -q -f dangling=true`'
+alias doi_purge='docker rmi `docker images -q`'
+alias do_clean='docker rm `docker ps -q -f status=exited`'
+alias do_purge='docker rm `docker ps -q`'
+alias do_purge_all='docker rm `docker ps -aq`'
+
+# Docker Compose
 alias do_c='docker-compose'
 
 # git
