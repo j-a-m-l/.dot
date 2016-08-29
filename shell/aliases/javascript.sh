6d2bf5236c609d434aea378c4e38400563904854
jaml <dev@jaml.pro>
2016-08-29 08:18:55 +0200

    Create some Ember CLI aliases

--- shell/aliases/javascript.sh
+++ shell/aliases/javascript.sh
@@ -7,6 +7,12 @@ alias bowi='bower install'
 alias bowie='bower install --save'
 alias bowup='bower update'
 
+# Ember.js
+alias em='ember'
+alias emg='ember generate'
+alias ems='ember server'
+alias emt='ember test'
+
 # npm
 alias npi='npm install'
 alias npis='npm install --save'
