cf67f885e1c269203fa66afc0b5189615438e9e3
jaml <dev@jaml.pro>
2018-12-15 11:26:51 +0100

    Rust: create the setup script

---
+++ rust/.setup/install.sh
@@ -0,0 +1,12 @@
+# rustup
+curl https://sh.rustup.rs -sSf | sh
+
+rustup component add rustfmt-preview
+
+cargo install cargo-watch
+
+# To add, remove and upgrade dependencies
+cargo install cargo-edit
+
+# Audit `Cargo.lock` crates
+cargo install cargo-audit
