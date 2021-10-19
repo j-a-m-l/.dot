04e6d7a6da0af40150a10d05d176db08a47438ac
Juan A. Martín Lucas <dev@jaml.pro>
2021-10-19 12:12:41 +0200

    improvement shell: Add more aliases for npm, quasar Yalc and Yarn

--- shell/aliases/javascript.sh
+++ shell/aliases/javascript.sh
@@ -1,7 +1,3 @@
-
-# Node.js
-alias js='node'
-
 # bower
 alias bowi='bower install'
 alias bowie='bower install --save'
@@ -15,22 +11,6 @@ alias ems='ember server'
 alias emt='ember test'
 alias emts='ember test --server'
 
-# Lerna
-alias ler='lerna'
-alias lerbo='lerna bootstrap'
-alias lerru='lerna run'
-alias lerrub='lerna run build'
-
-# NPM
-alias npi='npm install'
-alias npig='npm install --global'
-alias npis='npm install --save'
-alias npid='npm install --save-dev'
-alias npr='npm run'
-alias nprs='npm run-script'
-alias nps='npm start'
-alias npt='npm test'
-
 # Grunt
 alias gru='grunt'
 alias grus='grunt serve'
@@ -39,13 +19,56 @@ alias grus='grunt serve'
 alias gul='gulp'
 alias guls='gulp serve'
 
+# Lerna
+alias ler='lerna'
+alias lerbo='lerna bootstrap'
+alias lerru='lerna run'
+alias lerrub='lerna run build'
+
+# Node.js
+alias js='node'
+
+# NPM
+alias npi='npm install'
+alias npig='npm install --global'
+alias npis='npm install --save'
+alias npid='npm install --save-dev'
+alias npus='npm uninstall --save'
+alias npr='npm run'
+alias nprs='npm run-script'
+alias nps='npm start'
+alias npt='npm test'
+alias npaf='npm audit fix'
+
+# Quasar
+alias quad='quasar dev'
+
+# Yalc
+alias ylp='yalc push'
+
 # Yarn
 alias ya='yarn'
-alias yad='yarn add'
-alias yre='yarn remove'
-alias yru='yarn run'
-alias yate='yarn test'
+alias yabu='yarn build'
+alias yadd='yarn add'
+alias yaddd='yarn add --dev'
+alias yade='yarn dev'
+alias yare='yarn remove'
+alias yaru='yarn run'
+alias yasb='yarn storybook'
+alias yase='yarn serve'
+alias yat='yarn test'
 
-alias ytd='yarn test:debug'
-alias ytw='yarn test:watch'
-alias ytuw='yarn test:unit:watch'
+# Others that depend on the project (`test:watch` VS. `test-watch` for instance)
+
+alias npte='npm run-script test:e2e'
+alias nptu='npm run-script test:unit'
+alias nptw='npm run-script test:watch'
+
+alias yatw='yarn test:watch'
+alias yatd='yarn test:debug'
+alias yate='yarn test:e2e'
+alias yatew='yarn test:e2e:watch'
+alias yatu='yarn test:unit'
+alias yatui='yarn test:unit:inspect'
+alias yatuiw='yarn test:unit:inspect:watch'
+alias yatuw='yarn test:unit:watch'
