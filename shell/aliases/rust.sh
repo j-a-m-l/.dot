0b4f7788b1ef3987ab2c157cb264e9135d83185d
Juan A. Martín Lucas <dev@jaml.pro>
2021-10-19 12:15:00 +0200

    feat shell: Create aliases for Diesel

--- shell/aliases/rust.sh
+++ shell/aliases/rust.sh
@@ -8,3 +8,12 @@ alias cgtn='cargo test -- --nocapture'
 # TODO clippy
 # TODO fmt
 alias cgw='cargo watch'
+
+# Diesel CLI
+alias di='diesel'
+alias did='diesel database'
+alias dim='diesel migration'
+alias dimd='diesel migration redo'
+alias dimr='diesel migration run'
+alias dimv='diesel migration revert'
+alias dimg='diesel migration generate'
