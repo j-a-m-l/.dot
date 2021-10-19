10f2f2fed65a12cfacfcb1ee03e3d67be07e0f85
Juan A. Martín Lucas <dev@jaml.pro>
2021-10-19 12:11:19 +0200

    improvement shell: Add more aliases for Docker and Docker Compose

--- shell/aliases/docker.sh
+++ shell/aliases/docker.sh
@@ -1,24 +1,38 @@
 # Docker
+alias do_='docker'
 alias dok='docker'
 alias dob='docker build'
 alias dop='docker ps'
 alias dopa='docker ps -a'
 alias doa='docker attach'
+alias doe='docker exect'
+alias doet='docker exect -it'
 alias dor='docker run'
 alias dori='docker run -it'
 
-alias doi='docker images'
-alias doia='docker images ls --all'
-alias doi_clean='docker rmi `docker images -q -f dangling=true`'
-alias doi_purge='docker rmi `docker images -q`'
-
-alias do_rm='docker rm -v'
-alias do_clean='docker rm `docker ps -q -f status=exited`'
 alias do_stop_all='docker stop `docker ps -aq`'
+alias do_rmv='docker rm -v'
+alias do_clean='docker rm `docker ps -q -f status=exited`'
 alias do_purge='docker rm `docker ps -q`'
 alias do_purge_all='docker rm `docker ps -aq`'
 
+alias doi='docker image'
+alias doi_l='docker image ls'
+alias doi_la='docker image ls --all'
+alias doi_rm='docker rmi'
+alias doi_rmf='docker rmi --force'
+alias doi_pa='docker image prune --all'
+alias doi_purge='docker rmi `docker image ls -q`'
+
 # Docker Compose
-alias do_c='docker-compose'
-alias do_cu='docker-compose up'
-alias do_cs='docker-compose stop'
+alias dc_='docker-compose' # To not override the `dc` command
+alias dc_u='docker-compose up'
+alias dc_d='docker-compose down'
+alias dc_dv='docker-compose down --volumes'
+alias dc_s='docker-compose stop'
+
+# TODO: better approach for auto-completions
+. /usr/share/bash-completion/completions/docker
+complete -F _docker do_
+. /usr/local/etc/bash_completion.d/docker-compose
+complete -F _docker_compose dc_
