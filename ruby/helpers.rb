2ae62fbb4d88e1aa1751d725fe6628b49b00672e
Juan A. Martín Lucas <dev@jaml.pro>
2021-10-19 12:32:49 +0200

    refactor Ruby: Use gpg command instead gpg2

--- ruby/helpers.rb
+++ ruby/helpers.rb
@@ -1,14 +1,19 @@
-# Toys
-class C; self; end
-def s; 'Example string' end
-def a; ['a', 'b', 'c', 'd'] end
-def h; { a: 'a', b: 'b', c: 'c', d: 'd' } end
-def hn; { a: { b: { c: { d: 'E' } } } } end
+# Fastest way to quit
+def q; exit end
 
+# Just in case
 def save_current_session path
   File.open path, 'w' do |f|
     f << Readline::HISTORY.to_a.join("\n")
   end
 end
 
-def q; exit end
+#
+# Toys for trying things fast
+#
+
+class C; self; end
+def s; 'Example string' end
+def a; ['a', 'b', 'c', 'd'] end
+def h; { a: 'A', b: 'B', c: 'C', d: 'D' } end
+def nh; { a: { b: { c: { d: 'D' } } } } end
